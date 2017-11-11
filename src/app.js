5f9e3e711d41b3b7f0e570e845a211165ea7cd72
Elvis Dedic <elvis26112009@gmail.com>
2017-11-11 18:38:41 +0100

    first logic

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-const p = 'elvis'
+import { Observable } from 'rxjs/Rx'
 
-console.log(p)
+import { showScore, clearCanvas } from './elements'
+import { sumLatest } from './pure'
+
+// CONSTANTS
+const ScoreInterval$ = Observable.interval(1000)
+
+function renderGameScene({ interval }) {
+    clearCanvas()
+    showScore(interval * 10)
+}
+
+const Game$ = Observable.combineLatest(
+    ScoreInterval$, (interval) => ({ interval })
+)
+
+Game$.subscribe(renderGameScene)
