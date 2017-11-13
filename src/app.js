58783e3168d90266e102e5434d33ff97001cc498
Elvis Dedic <elvis26112009@gmail.com>
2017-11-13 22:04:35 +0100

    testing some player input for text field

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import { Observable } from 'rxjs/Rx'
 
-import { canvas, showScore, clearCanvas, showPlayer } from './elements'
-import { byDirection, handlePlayerMovement } from './pure'
+import { canvas, input } from './elements'
+import { showScore, clearCanvas, showPlayer, showQuestion } from './elements'
+import { byDirection, handlePlayerMovement, byNumericalInput, handleUserInput } from './pure'
 
 // GAME RELATED CONSTANTS
 const PLAYER_Y_POSITION = canvas.height - 70
@@ -17,15 +18,22 @@ const PlayerMovement$ = Observable.fromEvent(document, 'keyup')
     .scan(handlePlayerMovement, PLAYER_STARTING_POSITION)
     .startWith(PLAYER_STARTING_POSITION)
 
-function renderGameScene({ interval, playerX }) {
-    clearCanvas()
-    showScore(interval * 10)
-    showPlayer({ x: playerX, y: PLAYER_Y_POSITION })
-}
+const PlayerInput$ = Observable.fromEvent(input, 'input')
+    .pluck('srcElement', 'value')
+    .scan((prev, curr) => !isNaN(curr) ? curr : prev, '')
+    .subscribe(e => input.value = e)
 
+// MAIN GAME OBSERVABLE
 const Game$ = Observable.combineLatest(
     ScoreInterval$, PlayerMovement$,
     (interval, playerX) => ({ interval, playerX })
 )
+
+function renderGameScene({ interval, playerX }) {
+    clearCanvas()
+    showScore(interval * 10)
+    showPlayer({ x: playerX, y: PLAYER_Y_POSITION })
+    showQuestion(12, 14, '+')
+}
 
 Game$.subscribe(renderGameScene)
