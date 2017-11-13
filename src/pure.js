e6cdfbba2b3d2583fe3475a063b3c86cae98478f
Elvis Dedic <elvis26112009@gmail.com>
2017-11-13 19:46:12 +0100

    pure functions byDirection and handlePlayerMovement

--- src/pure.js
+++ src/pure.js
@@ -1,5 +1,9 @@
 const sumLatest = (prev, curr) => prev + curr
+const byDirection = key => key === 'ArrowLeft' || key === 'ArrowRight'
+const handlePlayerMovement = (prevX, key) => prevX + (key === 'ArrowLeft' ? -10 : 10)
 
 module.exports = {
-    sumLatest
+    sumLatest,
+    byDirection,
+    handlePlayerMovement,
 }
