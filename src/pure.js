e7bb5e455eb821df178d2e20c29d05f51782e3a9
Elvis Dedic <elvis26112009@gmail.com>
2017-11-13 22:05:19 +0100

    testing new pure functions

--- src/pure.js
+++ src/pure.js
@@ -1,9 +1,19 @@
 const sumLatest = (prev, curr) => prev + curr
-const byDirection = key => key === 'ArrowLeft' || key === 'ArrowRight'
-const handlePlayerMovement = (prevX, key) => prevX + (key === 'ArrowLeft' ? -10 : 10)
+const byDirection = key => key === 'a' || key === 'd'
+const handlePlayerMovement = (prevX, key) => prevX + (key === 'a' ? -10 : 10)
+const byNumericalInput = value => value && !isNaN(value)
 
+const handleUserInput = ({ data, srcElement }, { value }) => {
+    console.log(data)
+    console.log(srcElement)
+    console.log(value)
+
+    return data + value
+}
 module.exports = {
     sumLatest,
     byDirection,
     handlePlayerMovement,
+    byNumericalInput,
+    handleUserInput,
 }
