6bf8dfb6da971666095a6fb4e949068176a64595
Elvis Dedic <elvis26112009@gmail.com>
2017-11-13 19:45:49 +0100

    introducing logic for player movment

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 import { Observable } from 'rxjs/Rx'
 
-import { showScore, clearCanvas } from './elements'
-import { sumLatest } from './pure'
+import { canvas, showScore, clearCanvas, showPlayer } from './elements'
+import { byDirection, handlePlayerMovement } from './pure'
 
-// CONSTANTS
+// GAME RELATED CONSTANTS
+const PLAYER_Y_POSITION = canvas.height - 70
+const PLAYER_STARTING_POSITION = canvas.width / 2
+
+// OBSERVABLE CONSTANTS
 const ScoreInterval$ = Observable.interval(1000)
 
-function renderGameScene({ interval }) {
+const PlayerMovement$ = Observable.fromEvent(document, 'keyup')
+    .merge(Observable.fromEvent(document, 'keydown'))
+    .pluck('key')
+    .filter(byDirection)
+    .scan(handlePlayerMovement, PLAYER_STARTING_POSITION)
+    .startWith(PLAYER_STARTING_POSITION)
+
+function renderGameScene({ interval, playerX }) {
     clearCanvas()
     showScore(interval * 10)
+    showPlayer({ x: playerX, y: PLAYER_Y_POSITION })
 }
 
 const Game$ = Observable.combineLatest(
-    ScoreInterval$, (interval) => ({ interval })
+    ScoreInterval$, PlayerMovement$,
+    (interval, playerX) => ({ interval, playerX })
 )
 
 Game$.subscribe(renderGameScene)
