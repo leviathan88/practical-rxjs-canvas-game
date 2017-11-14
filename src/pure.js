b96a992093c12e944f5e77580949bd11cb1d4339
Elvis Dedic <elvis26112009@gmail.com>
2017-11-14 20:34:35 +0100

    more filter functions

--- src/pure.js
+++ src/pure.js
@@ -2,6 +2,8 @@ const sumLatest = (prev, curr) => prev + curr
 const byDirection = key => key === 'a' || key === 'd'
 const handlePlayerMovement = (prevX, key) => prevX + (key === 'a' ? -10 : 10)
 const byNumericalInput = value => value && !isNaN(value)
+const byEnterPress = key => key === 'Enter'
+const byNotEmpty = input => !!input && input.trim().length > 0
 
 const handleUserInput = ({ data, srcElement }, { value }) => {
     console.log(data)
@@ -16,4 +18,6 @@ module.exports = {
     handlePlayerMovement,
     byNumericalInput,
     handleUserInput,
+    byEnterPress,
+    byNotEmpty
 }
