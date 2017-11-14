f9e15738ef6231afe03ceba9f27b11fe8b999e24
Elvis Dedic <elvis26112009@gmail.com>
2017-11-14 20:35:15 +0100

    remove unused func

--- src/pure.js
+++ src/pure.js
@@ -1,7 +1,6 @@
 const sumLatest = (prev, curr) => prev + curr
 const byDirection = key => key === 'a' || key === 'd'
 const handlePlayerMovement = (prevX, key) => prevX + (key === 'a' ? -10 : 10)
-const byNumericalInput = value => value && !isNaN(value)
 const byEnterPress = key => key === 'Enter'
 const byNotEmpty = input => !!input && input.trim().length > 0
 
@@ -16,7 +15,6 @@ module.exports = {
     sumLatest,
     byDirection,
     handlePlayerMovement,
-    byNumericalInput,
     handleUserInput,
     byEnterPress,
     byNotEmpty
