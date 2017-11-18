ada687c1631e34dd96b3d58f94c7f82dc5c54950
Elvis Dedic <elvis26112009@gmail.com>
2017-11-18 14:46:36 +0100

    move impure stuff to elements.js

--- src/pure.js
+++ src/pure.js
@@ -3,22 +3,6 @@ const byDirection = key => key === 'a' || key === 'd'
 const handlePlayerMovement = (prevX, key) => prevX + (key === 'a' ? -10 : 10)
 const byEnterPress = key => key === 'Enter'
 const byNotEmpty = input => !!input && input.trim().length > 0
-const operationsMap = new Map([
-    [0, '+'],
-    [1, '-'],
-    [2, '*'],
-    [3, '/']
-])
-
-const operations = {
-    getRandomOperationNumber: () => getRandomNumber(4),
-    getRandomLargeNumber: () => getRandomNumber(101),
-    getRandomSmallNumber: () => getRandomNumber(21),
-    getOperator: i => operationsMap.get(i),
-    calculate: o => performOperations(o),
-    getOperationObject: () => ({ r: operations.getOperator(operations.getRandomOperationNumber()), a: operations.getRandomSmallNumber(), b: operations.getRandomSmallNumber() })
-}
-
 
 module.exports = {
     sumLatest,
@@ -26,24 +10,4 @@ module.exports = {
     handlePlayerMovement,
     byEnterPress,
     byNotEmpty,
-    operations
-}
-
-function performOperations(operator) {
-    return function(a, b) {
-        switch (operator) {
-            case '*':
-                return a * b
-            case '/':
-                return a / b
-            case '+':
-                return a + b
-            case '-':
-                return a - b
-        }
-    }
-}
-
-function getRandomNumber(range) {
-    return parseInt(Math.random() * range)
 }
