8017d5f58331bd116f9d76128dc13831a56d755f
Elvis Dedic <elvis26112009@gmail.com>
2017-11-18 14:46:11 +0100

    final logic for score and calculation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
-import { Observable } from 'rxjs/Rx'
+import { Observable, BehaviorSubject } from 'rxjs/Rx'
 
 import { canvas, input } from './elements'
-import { showScore, clearCanvas, showPlayer, showQuestion, clearInput } from './elements'
-import { byDirection, handlePlayerMovement, byEnterPress, byNotEmpty, multiplyNumbers, operations } from './pure'
+import { showScore, clearCanvas, showPlayer, showQuestion, clearInput, operations } from './elements'
+import { byDirection, handlePlayerMovement, byEnterPress, byNotEmpty, multiplyNumbers, sumLatest } from './pure'
 
 // GAME RELATED CONSTANTS
 const PLAYER_Y_POSITION = canvas.height - 70
 const PLAYER_STARTING_POSITION = canvas.width / 2
+const OPERATIONS_INTERVAL_CHECK = 500
+const SCORE_INTERVAL_RAISE = 5000
+const SCORE_INTERVAL_POINTS = 100
+const SCORE_CORRECT_POINTS = 50
+const SCORE_PENALTY_POINTS = -50
+const OPERATION_LIFETIME = 5000
 
 // OBSERVABLE CONSTANTS
-const ScoreInterval$ = Observable.interval(1000)
+const CurrentOperationBehavior$ = new BehaviorSubject(operations.getOperationObject())
+const ScoreBehavior$ = new BehaviorSubject(0)
+const CurrentScore$ = ScoreBehavior$.scan(sumLatest)
+const ScoreInterval$ = Observable.interval(SCORE_INTERVAL_RAISE).map(() => SCORE_INTERVAL_POINTS)
 const PlayerEnterPress$ = Observable.fromEvent(input, 'keyup').pluck('key').filter(byEnterPress)
-const OperationsTimer$ = Observable.timer(0, 5000)
+const OperationsInterval$ = Observable.interval(500)
 
 const PlayerMovement$ = Observable.fromEvent(document, 'keyup')
     .merge(Observable.fromEvent(document, 'keydown'))
@@ -20,30 +29,31 @@ const PlayerMovement$ = Observable.fromEvent(document, 'keyup')
     .scan(handlePlayerMovement, PLAYER_STARTING_POSITION)
     .startWith(PLAYER_STARTING_POSITION)
 
-const LatestValue$ = PlayerEnterPress$
-    .switchMap(() => Observable.of(input.value))
+const LatestOperation$ = Observable.merge(
+        PlayerEnterPress$.map(value => ({ value })),
+        OperationsInterval$.map(() => ({ value: Date.now() }))
+    )
+    .filter(({ value }) => value === 'Enter' || (value - CurrentOperationBehavior$.getValue().timestamp >= OPERATION_LIFETIME))
+    .map(({ value }) => Number(input.value) === CurrentOperationBehavior$.getValue().result ? SCORE_CORRECT_POINTS : SCORE_PENALTY_POINTS)
     .do(clearInput)
+    .do(() => CurrentOperationBehavior$.next(operations.getOperationObject()))
 
-const NewOperation$ = Observable.merge(
-    PlayerEnterPress$,
-    OperationsTimer$
-).switchMap(() => Observable.of(operations.getOperationObject()))
-
-const CheckResults$ = LatestValue$.withLatestFrom(
-    NewOperation$
-).subscribe(console.log)
+Observable.merge(
+    ScoreInterval$,
+    LatestOperation$
+).subscribe(score => ScoreBehavior$.next(score))
 
 // MAIN GAME OBSERVABLE
 const Game$ = Observable.combineLatest(
-    ScoreInterval$, PlayerMovement$, NewOperation$,
-    (interval, playerX, o) => ({ interval, playerX, o })
+    CurrentScore$, PlayerMovement$, CurrentOperationBehavior$,
+    (score, playerX, operation) => ({ score, playerX, operation })
 )
 
-function renderGameScene({ interval, playerX, o }) {
+function renderGameScene({ score, playerX, operation }) {
     clearCanvas()
-    showScore(interval * 10)
+    showScore(score)
     showPlayer({ x: playerX, y: PLAYER_Y_POSITION })
-    showQuestion(o.a, o.b, o.r)
+    showQuestion(operation.a, operation.b, operation.operator)
 }
 
 Game$.subscribe(renderGameScene)
