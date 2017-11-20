0e1614a5282cab31f5f0ee6426aec91d286c297f
Elvis Dedic <elvis26112009@gmail.com>
2017-11-20 22:57:02 +0100

    reorder

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ import { showScore, clearCanvas } from './elements'
 // CONSTANTS
 const ScoreInterval$ = Observable.interval(1000)
 
-function renderGameScene({ interval }) {
-    clearCanvas()
-    showScore(interval * 10)
-}
-
 const Game$ = Observable.combineLatest(
     ScoreInterval$, (interval) => ({ interval })
 )
 
 Game$.subscribe(renderGameScene)
+
+function renderGameScene({ interval }) {
+    clearCanvas()
+    showScore(interval * 10)
+}
