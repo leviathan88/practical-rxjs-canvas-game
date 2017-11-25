280458d1ca63335a263705245d2165d40fcc7186
Elvis Dedic <elvis26112009@gmail.com>
2017-11-25 10:37:22 +0100

    use a and d

--- src/pure.js
+++ src/pure.js
@@ -1,6 +1,6 @@
 const sumLatest = (prev, curr) => prev + curr
-const byDirection = key => key === 'ArrowLeft' || key === 'ArrowRight'
-const handlePlayerMovement = (prevX, key) => prevX + (key === 'ArrowLeft' ? -10 : 10)
+const byDirection = key => key === 'a' || key === 'd'
+const handlePlayerMovement = (prevX, key) => prevX + (key === 'a' ? -10 : 10)
 
 module.exports = {
     sumLatest,
